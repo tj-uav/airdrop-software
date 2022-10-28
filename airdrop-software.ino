56414fe91ab240384dcf4e05f39a07368428f912
Nicolas-M1 <nickmakovnik@gmail.com>
2022-10-28 12:29:21 -0400

    Added GPS interfacing

--- airdrop-software.ino
+++ airdrop-software.ino
@@ -1,8 +1,18 @@
 #include <SPI.h>
 #include <SD.h>
+#include <MicroNMEA.h> //http://librarymanager/All#MicroNMEA
+#include <Wire.h> //Needed for I2C to GNSS
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
+
 
 String logFileName = "log.txt";
 const int CHIP_SELECT = 10;
+
+
+SFE_UBLOX_GNSS myGNSS;
+char nmeaBuffer[100];
+MicroNMEA nmea(nmeaBuffer, sizeof(nmeaBuffer));
 
 File logFile;
 
@@ -29,11 +39,25 @@ void setup() {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+  Wire.begin();
+
+  if (myGNSS.begin() == false)
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+  myGNSS.setI2COutput(COM_TYPE_UBX | COM_TYPE_NMEA); //Set the I2C port to output both NMEA and UBX messages
+  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
+
+  myGNSS.setProcessNMEAMask(SFE_UBLOX_FILTER_NMEA_ALL); // Make sure the library is passing all NMEA messages to processNMEA
+
+  myGNSS.setProcessNMEAMask(SFE_UBLOX_FILTER_NMEA_GGA); // Or, we can be kind to MicroNMEA and _only_ pass the GGA messages to it
 
 }
 
 
-void initSd(chip_select){
+void initSd(int chip_select){
   Serial.print("Initializing SD card...");
   pinMode(chip_select, OUTPUT);
   delay(100);
@@ -53,6 +77,37 @@ void logStr(String data){
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  myGNSS.checkUblox(); //See if new data is available. Process bytes as they come in.
 
+  if(nmea.isValid() == true)
+  {
+    long latitude_mdeg = nmea.getLatitude();
+    long longitude_mdeg = nmea.getLongitude();
+
+    Serial.print("Latitude (deg): ");
+    Serial.println(latitude_mdeg / 1000000., 6);
+    Serial.print("Longitude (deg): ");
+    Serial.println(longitude_mdeg / 1000000., 6);
+
+    nmea.clear(); // Clear the MicroNMEA storage to make sure we are getting fresh data
+  }
+  else
+  {
+    Serial.println("Waiting for fresh data");
+  }
+
+  delay(250); //Don't pound too hard on the I2C bus
+
+}
+
+
+//This function gets called from the SparkFun u-blox Arduino Library
+//As each NMEA character comes in you can specify what to do with it
+//Useful for passing to other libraries like tinyGPS, MicroNMEA, or even
+//a buffer, radio, etc.
+void SFE_UBLOX_GNSS::processNMEA(char incoming)
+{
+  //Take the incoming char from the u-blox I2C port and pass it on to the MicroNMEA lib
+  //for sentence cracking
+  nmea.process(incoming);
 }
